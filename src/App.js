d544c5b41878b19e045ffc7149d1d5d79e04d398
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-06-11 13:10:03 -0400

    lottery component completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Ball from './Ball';
+import Lottery from './Lottery';
 
 function App() {
   return (
     <div className="App">
-      <Ball num = {17}/>
+      <Lottery/>
+      <Lottery title="Mini Daily" maxBalls={4} maxNum={4}/>
     </div>
   );
 }
