09e070e086f4a82d68577a95eb9c5b5c39d0843c
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-06-11 10:13:22 -0400

    loto ball component completed, with styling, will change to account for number of balls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Ball from './Ball';
 
 function App() {
   return (
     <div className="App">
-      
-
+      <Ball num = {17}/>
     </div>
   );
 }
